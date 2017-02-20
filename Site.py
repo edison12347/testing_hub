43d6b67b1b42eb37ae31ae7bd5221c7867cc0e2d
edison12347 <eduard.chudaikin@gmail.com>
2017-02-20 22:30:05 +0200

    To be continue...  Interate over dates  create a date list  replace dates in the string

--- Site.py
+++ Site.py
@@ -1,4 +1,6 @@
 import requests
+import datetime
+from datetime import datetime, date, time, timedelta
 
 r = str(requests.get('https://reentryvisa.inis.gov.ie/website/INISOA/IOA.nsf/(getApps4DT)?openagent&dt=20/02/2017&type=I&num=1&_=1486826956189'))
 
@@ -11,3 +13,11 @@ f = open('request.txt', 'r+')
 f.write(r)
 
 print(f.read())
+
+start_date = datetime.strptime("20/02/2017", '%d/%m/%Y')
+end_date = datetime.strptime("23/02/2017", '%d/%m/%Y')
+
+d = date.today() + timedelta(days = 1)
+d = datetime.strptime('20-02-2017', '%d-%m-%Y')+ timedelta(days = 1)
+
+print(d)
