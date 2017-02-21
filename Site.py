6c7df92b480df9bf1976d9fa6b3d06a9b469f9b9
zamai-mbp <zamaialexander@gmail.com>
2017-02-21 08:56:29 +0000

    minor remarks
    Good job overall, added few remarks.

--- Site.py
+++ Site.py
@@ -6,12 +6,12 @@ f = open('request.txt', 'r+')
 
 print(f.read())
 
-START_DATE = datetime.strptime("21/02/2017", '%d/%m/%Y')
+START_DATE = datetime.strptime("21/02/2017", '%d/%m/%Y')  # excange the hardcoded date for today, datatitme.today()
 DAYS2CHECK = 10
 #end_date = datetime.strptime("23/02/2017", '%d/%m/%Y')
 
-for i in range(1,DAYS2CHECK):
-    d = START_DATE + timedelta(days = i)
-    d = d.strftime('%d/%m/%Y')
-    r = requests.get('https://reentryvisa.inis.gov.ie/website/INISOA/IOA.nsf/(getApps4DT)?openagent&dt='+ d +'&type=I&num=1&_=1486826956189')
+for i in range(1, DAYS2CHECK):
+    date = START_DATE + timedelta(days = i)
+    date = date.strftime('%d/%m/%Y')
+    r = requests.get('https://reentryvisa.inis.gov.ie/website/INISOA/IOA.nsf/(getApps4DT)?openagent&dt='+ date +'&type=I&num=1&_=1486826956189')  # string has a .format method better use it
     f.write(str(d)+ " : " + str(r.text) + '\n')
