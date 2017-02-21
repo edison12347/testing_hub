e9033582ecab11012542a9c7e9d67174929cbe1f
edison12347 <eduard.chudaikin@gmail.com>
2017-02-21 13:53:43 +0200

    Working requests with output into file

--- Site.py
+++ Site.py
@@ -7,13 +7,13 @@ f = open('request.txt', 'r+')
 
 print(f.read())
 
-START_DATE = datetime.strptime("21/02/2017", '%d/%m/%Y')
-DAYS2CHECK = 2
+START_DATE = datetime.today()
+DAYS2CHECK = 10
 #end_date = datetime.strptime("23/02/2017", '%d/%m/%Y')
 while True:
     for i in range(1,DAYS2CHECK):
         d = START_DATE + timedelta(days = i)
         d = d.strftime('%d/%m/%Y')
-        r = requests.get('https://reentryvisa.inis.gov.ie/website/INISOA/IOA.nsf/(getApps4DT)?openagent&dt='+ d +'&type=I&num=1&_=1486826956189')
+        r = requests.get('https://reentryvisa.inis.gov.ie/website/INISOA/IOA.nsf/(getApps4DT)?openagent&dt={}&type=I&num=1&_=1486826956189'.format(d))
         f.write(str(d)+ " : " + str(r.text) + '\n')
     time.sleep(60)
